6a4b114a7c6de3340e67301d0ca0731663104042
saiBhandarwad <mrsaibhandarwad@gmail.com>
2023-09-26 08:10:33 +0530

    dist folder upload

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const userRouter = require('./routes/userRouter')
 const { urlencoded } = require('body-parser')
 const auth = require('./middlewares/auth')
 const cors = require('cors')
+const path = require('path')
 const notesRouter = require('./routes/notesRouter')
 const server = express()
 const port = 8080
@@ -13,10 +14,18 @@ connectToMongo()
 
 server.use(express.json())
 server.use(cors())
+server.use(express.static('dist'))
+
 // server.use(urlencoded({extended:true}))
 
 server.use('/user', userRouter)
 server.use('/notes',auth, notesRouter)
+
+server.get('/*', (req,res)=>{
+    const filePath = path.resolve(__dirname,'./dist/index.html')
+    console.log(filePath);
+    res.sendFile(filePath)
+})
 
 server.listen(port, () => {
     console.log(`notesaver app listning at port : ${port}`);
